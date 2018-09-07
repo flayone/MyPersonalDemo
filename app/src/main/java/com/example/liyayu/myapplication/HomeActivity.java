9a1d46f0238be41d89fb12ada6e0014bc91b3682
689_100006206049_mr6 <689_100006206049_mr6@git.cloud.tencent.com>
2018-09-07 13:27:38 +0800

    添加蓝牙连接打印机demo

--- app/src/main/java/com/example/liyayu/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/liyayu/myapplication/HomeActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 
 import com.example.liyayu.myapplication.baseFramework.BaseKotlinActivity;
+import com.example.liyayu.myapplication.demoViews.bluetoothPrinterDemo.BluetoothActivity;
 import com.example.liyayu.myapplication.demoViews.coordinatorlayoutDemo.CoordinatorLayoutActivity;
 import com.example.liyayu.myapplication.demoViews.fontDemo.FontActivity;
 import com.example.liyayu.myapplication.demoViews.hotfixRobustDemo.RobustMainActivity;
@@ -50,6 +51,8 @@ public class HomeActivity extends BaseKotlinActivity {
     AppCompatButton button7;
     @BindView(R.id.button8)
     AppCompatButton button8;
+    @BindView(R.id.button9)
+    AppCompatButton button9;
     @BindView(R.id.button_end)
     AppCompatButton buttonEnd;
     Intent intent;
@@ -60,11 +63,11 @@ public class HomeActivity extends BaseKotlinActivity {
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         setJpushAlias(this, NormalUtilsKt.getVersionName(this));
-        Coloring.get().setViewRipple(text2, button1, button2, button4, button5, button6, button7, button8, buttonEnd);
+        Coloring.get().setViewRipple(text2, button1, button2, button4, button5, button6, button7, button8,button9, buttonEnd);
     }
 
     @OnClick({R.id.text1, R.id.text2, R.id.button1, R.id.button2, R.id.button4,
-            R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button_end})
+            R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9, R.id.button_end})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.text1:
@@ -116,6 +119,9 @@ public class HomeActivity extends BaseKotlinActivity {
                 break;
             case R.id.button8:
                 startAct(RobustMainActivity.class);
+                break;
+            case R.id.button9:
+                startAct(BluetoothActivity.class);
                 break;
             case R.id.button_end:
                 intent = new Intent(this, FontActivity.class);
